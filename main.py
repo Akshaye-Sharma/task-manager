1697850f914e7d5e4a73ee8e544b74cd05178070
Akshaye Sharma <akshaye.bhatt@gmail.com>
2025-05-26 16:20:48 +0100

    Documentation added in python file.

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import click
+"""
+main.py - A CLI tool for managing tasks in memory.
+
+Commands:
+    add, list, edit, delete
+"""
 
 # Simple in memory task list
 tasks = []
@@ -12,14 +18,26 @@ def cli():
 @cli.command()
 @click.argument('description')
 def add(description):
-    """ Adding a new task. """
+    """
+    Add a new task to the list.
+
+    :param description: The task to be added.
+    :type description: str
+    :return: none
+    """
     tasks.append(description)
     click.echo(f"Task added: {description}")
 
 @cli.command()
 @click.argument('task_id', type=int)
 def delete(task_id):
-    """ Deleting a task. """
+    """ 
+    Deleting a task from the list. 
+    
+    :param task_id: The task to be deleted.
+    :type task_id: int
+    :return: none
+    """
     if not tasks:
         click.echo("No tasks found.")
         return
@@ -33,7 +51,15 @@ def delete(task_id):
 @click.argument('task_id', type=int)
 @click.argument('new_description')
 def edit(task_id, new_description):
-    """ Edit an existing task. """
+    """
+    Edit an existing task. 
+    
+    :param task_id: The task being edited.
+    :type task_id: int
+    :param new_description: The updated task.
+    :type new_description: int
+    :return: none
+    """
     if 1 <= task_id <= len(tasks):
         tasks[task_id - 1] = new_description
         click.echo(f"Task {task_id} updated.")
@@ -42,13 +68,16 @@ def edit(task_id, new_description):
 
 @cli.command()
 def list():
-    """ List all current tasks. """
+    """
+    List all tasks.
+    
+    :return: none
+    """
     if not tasks:
         click.echo("No tasks found.")
-        return
-    
-    for i, task in enumerate(tasks, 1):
-        click.echo("f{i}. {task}")
+    else:
+        for i, task in enumerate(tasks, 1):
+            click.echo(f"{i}. {task}")
 
 if __name__ == "__main__":
     cli()
